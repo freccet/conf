24a61d1eb5d8472522aaa197026b812c2f446c5b
Evgeniy Kovalev <e.kovalevnsk@gmail.com>
2020-05-21 23:41:49 +0300

    ubuntu initial

---
+++ ubuntu/i3-gaps.sh
@@ -0,0 +1,17 @@
+cd /tmp
+git clone https://github.com/Airblader/xcb-util-xrm
+cd xcb-util-xrm
+git submodule update --init
+./autogen.sh --prefix=/usr
+make
+sudo make install
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+autoreconf --force --install
+rm -rf build
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
---
+++ ubuntu/software.sh
@@ -0,0 +1,6 @@
+#sudo add-apt-repository ppa:mmstick76/alacritty
+sudo apt install \
+ranger neovim i3 git zsh redshift qutebrowser alacritty rofi feh
+chsh --shell /bin/zsh $(whoami)
+#cp -R ../.config ~/
+

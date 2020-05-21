74ecd876a89f54f6af7fbe398dcdfe8b3c4fc685
Evgeniy Kovalev <e.kovalevnsk@gmail.com>
2020-05-22 00:43:14 +0300

    added software

--- ubuntu/software.sh
+++ ubuntu/software.sh
@@ -1,6 +1,7 @@
 #sudo add-apt-repository ppa:mmstick76/alacritty
 sudo apt install \
-ranger neovim i3 git zsh redshift qutebrowser alacritty rofi feh
-chsh --shell /bin/zsh $(whoami)
+ranger neovim i3 git zsh redshift qutebrowser alacritty rofi feh compton
+#chsh --shell /bin/zsh $(whoami)
 #cp -R ../.config ~/
+#cd ~/.config/qutebrowser && git clone https://github.com/dracula/qutebrowser-dracula-theme.git dracula
 
